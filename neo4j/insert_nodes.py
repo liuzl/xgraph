950fd85b0c8c1cb0e4af20b3130ddd12b9f8c371
liuzl <zhanliangliu@qq.com>
2019-05-26 15:17:20 +0800

    improve performance

--- neo4j/insert_nodes.py
+++ neo4j/insert_nodes.py
@@ -4,20 +4,26 @@ from tqdm import tqdm
 
 url = "http://localhost:7474/db/data/cypher"
 
+records = []
 def save(record):
+    global records
+
     if 'TYPE' in record:
-        t = "n:%s" % ':'.join(record['TYPE'])
+        t = ":%s" % ':'.join(record['TYPE'])
     else: return
     attrs = []
     for k, v in record.items():
         if k == 'TYPE': continue
         one = "%s:%s" % (k, json.dumps("\t".join(v), ensure_ascii=False))
         attrs.append(one)
-    cql = "CREATE (%s {%s})" % (t, ','.join(attrs))
+    cql = "(%s {%s})" % (t, ','.join(attrs))
+    records.append(cql)
     
-    q = {'query': cql, 'params':{}}
-    qstr = json.dumps(q, ensure_ascii=False).encode("utf-8")
-    ret = requests.post(url, data=qstr, auth=("neo4j", "123"))
+    if len(records) >= 100:
+        q = {'query': "CREATE " + ','.join(records), 'params':{}}
+        qstr = json.dumps(q, ensure_ascii=False).encode("utf-8")
+        ret = requests.post(url, data=qstr, auth=("neo4j", "123"))
+        records = []
 
 in_file = "nodes.txt"
 cnt = 0
@@ -40,4 +46,11 @@ for line in open(in_file):
         else: record[tag] = set([items[2],])
 if record is not None:
     save(record)
+
+if len(records) > 0:
+    q = {'query': ','.join(records), 'params':{}}
+    qstr = json.dumps(q, ensure_ascii=False).encode("utf-8")
+    ret = requests.post(url, data=qstr, auth=("neo4j", "123"))
+    records = []
+
 pbar.close()
